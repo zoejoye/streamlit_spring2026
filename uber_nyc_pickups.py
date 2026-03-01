6bf03a0453bb7e5fd003c022da4870b867e2ff1d
zoejoye <zoethinkera@gmail.com>
2026-03-01 15:25:05 -0600

    Update uber_nyc_pickups.py

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -35,7 +35,5 @@ st.bar_chart(hist_values)
 
 hour_to_filter = 17
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-
 st.map(filtered_data)
