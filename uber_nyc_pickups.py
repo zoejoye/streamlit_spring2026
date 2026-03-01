ac026566d70024e5e8fc05add0978504c232fa23
zoejoye <zoethinkera@gmail.com>
2026-03-01 15:31:20 -0600

    Update uber_nyc_pickups.py

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -29,10 +29,11 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+if st.checkbox('Show bar chart'):
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(
+        data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
