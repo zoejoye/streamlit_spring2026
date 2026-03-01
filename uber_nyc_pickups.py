f5dfe04c385b03c300fd13c187fd9b42dccec7bc
zoejoye <zoethinkera@gmail.com>
2026-03-01 15:29:14 -0600

    Update uber_nyc_pickups.py

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -25,8 +25,9 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded. 
 data_load_state.text("Done! (using st.cache_data)")
 
-st.subheader('Raw data')
-st.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
