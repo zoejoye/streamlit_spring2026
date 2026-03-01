e2a83d3fbadd443a0af401224ac1a00189fdb324
zoejoye <zoethinkera@gmail.com>
2026-03-01 15:03:02 -0600

    Update uber_nyc_pickups.py

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -26,5 +26,11 @@ data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
 
 st.subheader('Raw data')
-
 st.write(data)
+
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
