94a9ab097721f98f70f7376dea3a77ebfb092a29
zoejoye <zoethinkera@gmail.com>
2026-03-01 15:21:36 -0600

    Update uber_nyc_pickups.py

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -29,10 +29,8 @@ st.subheader('Raw data')
 st.write(data)
 
 st.subheader('Number of pickups by hour')
-
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
 st.bar_chart(hist_values)
 
 st.subheader('Map of all pickups')
